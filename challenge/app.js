ffec2b02b0d6a15b4623041a84ce39010cf7180b
deepak05kr18 <122279122+deepak05kr18@users.noreply.github.com>
2023-03-27 19:34:31 +0530

    lodash added

--- challenge/app.js
+++ challenge/app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+var _ = require('lodash');
 const ejs = require("ejs");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -36,6 +37,22 @@ app.get("/contact", function(req , res){
 app.get("/compose", function(req , res){
   res.render("compose" )
 });
+
+
+app.get("/posts/:topic", function(req , res){
+ const requestedTitle = _.lowerCase(req.params.topic);
+
+ posts.forEach(function(post){
+  const storedTitle =_.lowerCase( post.title);
+
+  if (storedTitle === requestedTitle){
+    console.log("Match Found");
+  }else{
+    console.log("Match Not Found")
+  }
+ });
+});
+
 
 app.post("/compose", function(req, res){
  const post={
